1114cb69e77f1849ab2d09bccca5880b3f073d9f
maissaAmg <109427534+maissaAmg@users.noreply.github.com>
2023-01-16 09:51:08 +0100

    sonar commit 9

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -115,8 +115,9 @@ public class MatrixMathematics {
 	 * @throws NoSquareException
 	 */
 	public static Matrix inverse(Matrix matrix) throws NoSquareException {
-		if(determinant(matrix) != 0){
-			return (transpose(cofactor(matrix)).multiplyByConstant(1.0/determinant(matrix)));
+		double i = determinant(matrix);
+		if(i != 0){
+			return (transpose(cofactor(matrix)).multiplyByConstant(1.0/i);
 		}
 		else{
 			return null;
